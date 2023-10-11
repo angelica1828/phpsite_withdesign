3e69fa2bf8c82417e81199571f38917e87728238
Angelica Sebastian <89622698+angelica1828@users.noreply.github.com>
2023-10-12 01:14:20 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+if(isset($_SESSION['id']) && isset($_SESSION['username'])){ 
+?>
+<!DOCTYPE html>
+<head>
+    <title>
+        HOME 
+    </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+   <h1>Hello, <?php echo $_SESSION['name']; ?> </h1><br> 
+   <a href="logout.php">LOGOUT</a>
+</body>
+</html>
+<?php
+}
+else{
+    header("Location:index.php");
+    exit();
+}
